6e1e34a266659a42df8387b72057cc2e91a94022
Pablo Eduardo Merino <3802987+eduardoaxml@users.noreply.github.com>
2023-01-19 22:23:01 -0600

    Blueprints

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from flask import request, make_response, redirect, render_template, session, url_for, flash
 import unittest
 
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
-app.config['SECRET_KEY'] = '345SDFGDSGAS!$SDG'
+from app import create_app
+from app.forms import LoginForm
+
+app = create_app()
+
+
 todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
 
-class LoginForm(FlaskForm):
-    username = StringField('Nombre de usuario', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Enviar')
 
 @app.cli.command()
 def test():
@@ -24,6 +19,7 @@ def test():
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html', error=error)
+
 
 @app.errorhandler(500)
 def server_error(error):
@@ -62,3 +58,4 @@ def hello():
         return redirect(url_for('index'))
 
     return render_template('hello.html', **context)
+
