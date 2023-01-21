755e43af13fea9a154450315b1d0548ee6ebc63c
Pablo Eduardo Merino <3802987+eduardoaxml@users.noreply.github.com>
2023-01-20 18:55:08 -0600

    Login

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-from flask import request, make_response, redirect, render_template, session, url_for, flash
 import unittest
+from flask import request, make_response, redirect, render_template, session, url_for, flash
+from flask_login import login_required, current_user
+
 
 from app import create_app
 from app.forms import LoginForm
 from app.firestore_service import get_users, get_todos
 
-
 app = create_app()
-
-
-todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
 
 
 @app.cli.command()
@@ -39,20 +37,15 @@ def index():
 
 
 @app.route('/hello', methods=['GET'])
+@login_required
 def hello():
     user_ip = session.get('user_ip')
-    username = session.get('username')
+    username = current_user.id
 
     context = {
         'user_ip': user_ip,
         'todos': get_todos(user_id=username),
         'username': username
     }
-    users = get_users()
-
-    for user in users:
-        print(user.id)
-        print(user.to_dict()['password'])
 
     return render_template('hello.html', **context)
-
